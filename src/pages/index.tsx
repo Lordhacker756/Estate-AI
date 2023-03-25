3fea7704047711a12a431fb2b671fe18a91b1640
Utkarsh <utkarshmishra2001@gmail.com>
2023-03-26 00:47:42 +0530

    fixed static file serving error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "../assets/logo.png";
-import heroBg from "../assets/hero.png";
+import logo from "../public/logo.png";
+import heroBg from "../public/hero.png";
 
 export default function Home() {
   return (
